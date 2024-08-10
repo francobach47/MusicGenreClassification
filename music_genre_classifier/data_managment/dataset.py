38f7c810f2a4678a41abea561654777fce535d4e
francobach47 <francobach47@gmail.com>
2024-08-10 16:45:27 -0300

    update: train model script

--- music_genre_classifier/data_managment/dataset.py
+++ music_genre_classifier/data_managment/dataset.py
@@ -4,8 +4,6 @@ import numpy as np
 from natsort import natsorted
 from torch.utils.data import Dataset, DataLoader, random_split
 
-DATASET_PATH = "dataset/genres_mel_npz"
-MINIBATCH_SIZE = 64
 # Dataset split
 TRAIN_SET_PCT = 0.8
 VALID_SET_PCT = 0.15
@@ -24,58 +22,71 @@ genres_map = {
     9: "Rock",
 }
 
+
 # Dataset class
 class MusicGenreDataset(Dataset):
-    
-    def __init__(self, dataset_path: str = DATASET_PATH):
-        # Sorting npz files
-        npz_files = natsorted(os.listdir(dataset_path))
-        # Initialize attributes
+
+    def __init__(self, dataset_path: str):
+
+        dataset_path = get_data_path(dataset_path)
         mel_specs = []
         genre_gt = []
-        # Iterate through npz files
-        for gt_i, genre_i_npz in enumerate(npz_files):
-            genre_data = np.load(os.path.join(dataset_path, genre_i_npz))
-            # Iterate through mel specs in genre_i
-            for mel_spec_i in genre_data["array"]:
-                # Creating tensors from np.ndarrays
-                mel_spec_tensor = torch.from_numpy(mel_spec_i).float()
-                gt_i_tensor = torch.tensor(gt_i)
-                # Accumulating mel specs and gt 
-                mel_specs.append(mel_spec_tensor)
-                genre_gt.append(gt_i_tensor)
-        # Go from list of tensors to one tensor
+
+        for genre_dir in os.listdir(dataset_path):
+            genre_path = os.path.join(dataset_path, genre_dir)
+            npz_files = natsorted(os.listdir(genre_path))
+            for gt_i, genre_i_npz in enumerate(npz_files):
+                genre_data = np.load(os.path.join(genre_path, genre_i_npz))
+                for mel_spec_i in genre_data["spectrogram"]:
+                    mel_spec_tensor = torch.from_numpy(mel_spec_i).float()
+                    gt_i_tensor = torch.tensor(gt_i)
+                    mel_specs.append(mel_spec_tensor)
+                    genre_gt.append(gt_i_tensor)
         self.mel_specs = torch.stack(mel_specs)
         self.genre_gt = torch.stack(genre_gt)
 
-
     def __getitem__(self, index):
         return self.mel_specs[index], self.genre_gt[index]
-    
+
     def __len__(self):
         return len(self.genre_gt)
 
 
-music_genre_dataset = MusicGenreDataset()
+def get_train_loader(dataset_path: str, batch_size: int = 64) -> DataLoader:
+    dataset = MusicGenreDataset(dataset_path)
+    train_set, _, _ = random_split(
+        dataset=dataset,
+        lengths=[TRAIN_SET_PCT, VALID_SET_PCT, TEST_SET_PCT],
+        generator=torch.Generator().manual_seed(42),
+    )
+    return DataLoader(
+        dataset=train_set, batch_size=batch_size, shuffle=True, num_workers=4
+    )
 
-# Split dataset into train, validation and test set
-train_set, valid_set, test_set = random_split(
-    dataset=music_genre_dataset,
-    lengths=[TRAIN_SET_PCT,VALID_SET_PCT,TEST_SET_PCT],
-    generator=torch.Generator().manual_seed(42))
 
-# Dataloaders
-train_loader = DataLoader(dataset=train_set,
-                        batch_size=MINIBATCH_SIZE,
-                        shuffle=True,
-                        num_workers=4)
+def get_valid_loader(dataset_path: str, batch_size: int = 64) -> DataLoader:
+    dataset = MusicGenreDataset(dataset_path)
+    _, valid_set, _ = random_split(
+        dataset=dataset,
+        lengths=[TRAIN_SET_PCT, VALID_SET_PCT, TEST_SET_PCT],
+        generator=torch.Generator().manual_seed(42),
+    )
+    return DataLoader(
+        dataset=valid_set, batch_size=batch_size, shuffle=True, num_workers=4
+    )
 
-valid_loader = DataLoader(dataset=valid_set,
-                        batch_size=MINIBATCH_SIZE,
-                        shuffle=True,
-                        num_workers=4)
 
-test_loader = DataLoader(dataset=test_set,
-                        batch_size=MINIBATCH_SIZE,
-                        shuffle=True,
-                        num_workers=4)
+def get_test_loader(dataset_path: str, batch_size: int = 64) -> DataLoader:
+    dataset = MusicGenreDataset(dataset_path)
+    _, _, test_set = random_split(
+        dataset=dataset,
+        lengths=[TRAIN_SET_PCT, VALID_SET_PCT, TEST_SET_PCT],
+        generator=torch.Generator().manual_seed(42),
+    )
+    return DataLoader(
+        dataset=test_set, batch_size=batch_size, shuffle=True, num_workers=4
+    )
+
+
+def get_data_path(base_path):
+    return os.path.join(base_path, "genres_mel_npz")
